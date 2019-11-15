09828b15415101beb63f38f127970d661308b9d2
Ronan <rsf.sho@gmail.com>
2019-11-14 19:31:37 -0800

    added compression

--- index.js
+++ index.js
@@ -20,6 +20,18 @@ const aiServerPort = 8082
 let stationListConversion = 100
 let passPhraseCache = []
 const arrivalURLs = ['http://localhost:8080', 'https://arrival.stomprocket.io', 'https://app.arrival.city']
+const compression = require('compression')
+app.use(compression({ filter: shouldCompress }))
+
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
 app.use(cors())
 io.origins('*:*')
 app.use(bodyParser.urlencoded({extended: false}))
